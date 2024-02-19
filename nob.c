dae4743b80df267256418e90ab5fc28c45d4c40a
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-02-19 09:50:26 -0300

    fix

--- nob.c
+++ nob.c
@@ -2,12 +2,19 @@
 #include "./nob.h"
 
 #define CFLAGS "-Wall", "-Wextra", "-pedantic", "-std=c11"
-#define LIBS "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11"
+#ifdef __WIN32
+    #define LIBS "-lraylib", "-lopengl32", "-lgdi32", "-lwinmm"
+#else
+    #define LIBS "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11"
+#endif // _WIN32
 #define TARGET "build/snake"
 
 void build_src(const char *src)
 {
     Cstr src_path = PATH("src", src);
+    if (!path_exists("build")) {
+        path_mkdirs(cstr_array_make("build"));
+    }
     CMD("cc", CFLAGS, "-o", TARGET, src_path, LIBS);
 }
 
