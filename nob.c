1d7800cac05bfdf874af9224f1bb015ffce68c7e
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-02-19 10:03:02 -0300

    FIX

--- nob.c
+++ nob.c
@@ -4,8 +4,10 @@
 #define CFLAGS "-Wall", "-Wextra", "-pedantic", "-std=c11"
 #ifdef __WIN32
     #define LIBS "-lraylib", "-lopengl32", "-lgdi32", "-lwinmm"
+    #define BUILD "build"
 #else
     #define LIBS "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11"
+    #define BUILD "build", 0755
 #endif // _WIN32
 #define TARGET "build/snake"
 
@@ -13,7 +15,7 @@ void build_src(const char *src)
 {
     Cstr src_path = PATH("src", src);
     if (!path_exists("build")) {
-        mkdir("build");
+        mkdir(BUILD);
     }
     CMD("cc", CFLAGS, "-o", TARGET, src_path, LIBS);
 }
